43eaa8033db2e1cfb5359fbae3445eb1a1bf3963
paulbrowne <Paulmatthewbrowne@gmail.com>
2018-07-15 11:43:49 +0300

    adds falsey and undefined test for window obj

--- clr.js
+++ clr.js
@@ -12,9 +12,11 @@
             for (key in resources) {
 
                 // check if the selector exists in the DOM
-                // or is a truthy object of the window
+                // or is a truthy object of the window, eg window.abc = true or window.abc = 42 or window.abc = "hello world!" etc
+                // or, if "!" is found, is a falsey object of the window, eg window.abc = false or window.abc = 0
+                // or, if "!!" is found, is falsey or undefined, eg window.abc = undefined
 
-                if (document.querySelector(key) || window[key]) {
+                if ((!~key.indexOf('!') && (document.querySelector(key) || window[key])) || (~key.indexOf('!') && window[key.slice("1")] == false) || (~key.indexOf('!!') && !window[key.slice("2")])) {
 
                     // if so, iterate over the array of resources for that selector
 
